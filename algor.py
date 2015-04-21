e81f077025b20a6457352bf6748bfc4d1eeeaf61
bwoka <brianwachowicz@gmail.com>
2015-04-21 18:40:11 -0400

    added skeleton for api interface to algo

--- algor.py
+++ algor.py
@@ -30,6 +30,20 @@ class schedule:
 
 
 
+#takes an list of single_class objects and returns a
+#dictionary with single_class keys mapping to the corresponding time_period list
+#inputted invalid classes return empty lists
+def get_schedule(classes):
+	result={}
+	for class in classes:
+		classInfo=1#get info here
+		parsed=1#parse here
+		result[class.name]=parsed
+	return result
+
+
+
+
 
 #outputs : top 10 best schedules ranked
 # iput priority dictionary of classes, RAW from Api
